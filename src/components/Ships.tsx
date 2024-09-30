150224a7657eea2e795fd01f417fce7061235513
karystosecret <karystosecret@gmail.com>
2024-10-01 01:20:50 +0300

    third commit2

--- src/components/Ships.tsx
+++ src/components/Ships.tsx
@@ -46,7 +46,7 @@ const Shops = () =>  {
         <div id="Ships" className="animate-fadeInAnimation p-4 md:p-20 bg-gradient-to-r from-[#a0b29d] to-[#7ea9b5]  z-0 w-full md:pt-8 overflow-hidden pb-40 md:pb-40">
         <Image src={olivesImage} width={100} height={100} layout="intrinsic" sizes=""  alt=''className="hidden scale-75 md:scale-100 drop-shadow-md absolute right-0 -mt-8 md:mt-0 mr-0 md:mr-6 z-20"/>
         <p className="text-4xl md:text-5xl text-white font-bold text-center  mb-12 z-10 drop-shadow-md tracking-tight">Δρομολόγια Πλοίων</p>
-        <p className="text-lg font-semibold text-white text-center mt-8">Ραφήνα - Μαρμάρι</p>
+        <p className="text-lg font-semibold text-white text-center mt-0">Ραφήνα - Μαρμάρι</p>
         <div className="grid w-full shadow-xl rounded-md items-center z-10 mt-2 md:mt-4">
             <div className="grid grid-cols-8 grid rows-3 z-10">
                 <div className="w-full md:w-40"></div>
