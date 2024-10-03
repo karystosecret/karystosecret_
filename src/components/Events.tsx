4d63ed32ae3fe45030bc34e4e276f45c8e90e51e
karystosecret <karystosecret@gmail.com>
2024-10-03 22:08:36 +0300

    third commit2

--- src/components/Events.tsx
+++ src/components/Events.tsx
@@ -20,10 +20,10 @@ export default function EmblaCarousel() {
 
   return (
     <div id='Events' className='p-4 md:p-20 animate-fadeInAnimation embla w-full bg-gradient-to-r from-[#a0b29d] to-[#7ea9b5] md:pt-8 pb-40'>
-        <p className="text-4xl md:text-5xl text-white font-bold text-center mb-8 drop-shadow-md tracking-tight">Εκδηλώσεις</p>
+        <p className="text-4xl md:text-5xl text-white font-bold text-center mb-4 drop-shadow-md tracking-tight">Εκδηλώσεις</p>
         <div className='animate-moveDownAnimation flex flex-col md:flex-row rounded-md shadow-md md:shadow-xl md:px-44 bg-white bg-opacity-10 md:p-8'>
-          <p className="mx-0 mt-4 md:mt-0 md:mx-4 md:m-auto  md:w-3/6 m-0 text-gray-200 center text-center text-sm drop-shadow-md">Συναυλίες, εκθέσεις, θεατρικές και χορευτικές παραστάσεις και προβολές, διοργανώνονται στην Κάρυστο, προσφέροντας μια μοναδική εμπειρία.</p>
-          <div className='embla__viewport md:w-[30%] w-3/4 m-8 mt-4 md:mt-8 md:m-auto shadow-xl mx-auto rounded-md overflow-hidden' ref={emblaRef}>
+          <p className="mx-0 mt-0 md:mt-1/3 md:mx-4 md:m-auto  md:w-3/6 m-0 text-gray-200 center text-center text-sm drop-shadow-md">Συναυλίες, εκθέσεις, θεατρικές και χορευτικές παραστάσεις και προβολές, διοργανώνονται στην Κάρυστο, προσφέροντας μια μοναδική εμπειρία.</p>
+          <div className='embla__viewport md:w-[30%] w-3/4 m-8 mt-4 md:mt-4 md:m-auto shadow-xl mx-auto rounded-md overflow-hidden' ref={emblaRef}>
             <div className='embla__container h-full '>
               <div className='embla__slide flex items-center justify-center'>
                 <Image className='rounded-lg ' src={Image1} width={0} height={0} sizes="100vw" style={{ width: '100%', height: 'auto' }} alt=""></Image>
