60f07211d8de3d58d891b0f5fe724985147cbaeb
karystosecret <karystosecret@gmail.com>
2024-11-23 01:37:02 +0200

    third commit2

--- src/components/Ships.tsx
+++ src/components/Ships.tsx
@@ -55,26 +55,26 @@ const Shops = () =>  {
                 <p className="text-[6px] md:text-xs text-black outline outline-1 md:outline-2 outline-white p-1 text-center">Σάββατο</p>
                 <p className="text-[6px] md:text-xs text-black outline outline-1 md:outline-2 outline-white p-1 rounded-tr-md text-center">Κυριακή</p>
                 
-                <p className="text-[6px] md:text-xs text-black outline outline-1 md:outline-2 outline-white p-1 rounded-tl-md text-center">Ραφήνα-Μαρμάρι </p>
-                <p className="text-[8px] md:text-xs outline outline-1 md:outline-2 outline-white p-2 text-center" >9:00, 16:00</p>
-                <p className="text-[8px] md:text-xs outline outline-1 md:outline-2 outline-white p-2 text-center">9:00, 16:00</p>
-                <p className="text-[8px] md:text-xs outline outline-1 md:outline-2 outline-white p-2 text-center">9:00, 16:00</p>
-                <p className="text-[8px] md:text-xs outline outline-1 md:outline-2 outline-white p-2 text-center">9:00, 16:00</p>
-                <p className="text-[8px] md:text-xs outline outline-1 md:outline-2 outline-white p-2 text-center">9:00, 16:00</p>
-                <p className="text-[8px] md:text-xs outline outline-1 md:outline-2 outline-white p-2 text-center">9:00, 16:00</p>
-                <p className="text-[8px] md:text-xs outline outline-1 md:outline-2 outline-white p-2 text-center">9:00, 16:00</p>
+                <p className="text-[6px] md:text-xs text-black outline outline-1 md:outline-2 outline-white p-1 rounded-tl-md text-center">Ραφήνα-Μαρμάρι</p>
+                <p className="text-[8px] md:text-xs outline outline-1 md:outline-2 outline-white p-2 text-center" >08:30, 17:15</p>
+                <p className="text-[8px] md:text-xs outline outline-1 md:outline-2 outline-white p-2 text-center">08:30, 17:00</p>
+                <p className="text-[8px] md:text-xs outline outline-1 md:outline-2 outline-white p-2 text-center">08:30, 17:00</p>
+                <p className="text-[8px] md:text-xs outline outline-1 md:outline-2 outline-white p-2 text-center">08:30, 17:00</p>
+                <p className="text-[8px] md:text-xs outline outline-1 md:outline-2 outline-white p-2 text-center">08:30, 18:00</p>
+                <p className="text-[8px] md:text-xs outline outline-1 md:outline-2 outline-white p-2 text-center">08:30, 12:00, 17:00</p>
+                <p className="text-[8px] md:text-xs outline outline-1 md:outline-2 outline-white p-2 text-center">10:00, 18:00</p>
 
-                <p className="text-[6px] md:text-xs text-black outline outline-1 md:outline-2 outline-white p-1 rounded-bl-md text-center">Μαρμάρι-Ραφήνα </p>
-                <p className="text-[8px] md:text-xs outline outline-1 md:outline-2 outline-white p-2 text-center">9:00, 16:00</p>
-                <p className="text-[8px] md:text-xs outline outline-1 md:outline-2 outline-white p-2 text-center">9:00, 16:00</p>
-                <p className="text-[8px] md:text-xs outline outline-1 md:outline-2 outline-white p-2 text-center">9:00, 16:00</p>
-                <p className="text-[8px] md:text-xs outline outline-1 md:outline-2 outline-white p-2 text-center">9:00, 16:00</p>
-                <p className="text-[8px] md:text-xs outline outline-1 md:outline-2 outline-white p-2 text-center">9:00, 16:00</p>
-                <p className="text-[8px] md:text-xs outline outline-1 md:outline-2 outline-white p-2 text-center">9:00, 16:00</p>
-                <p className="text-[8px] md:text-xs outline outline-1 md:outline-2 outline-white p-2 rounded-br-md text-center" >9:00, 16:00</p>
+                <p className="text-[6px] md:text-xs text-black outline outline-1 md:outline-2 outline-white p-1 rounded-bl-md text-center">Μαρμάρι-Ραφήνα</p>
+                <p className="text-[8px] md:text-xs outline outline-1 md:outline-2 outline-white p-2 text-center">06:45, 15:00</p>
+                <p className="text-[8px] md:text-xs outline outline-1 md:outline-2 outline-white p-2 text-center">06:45, 15:30</p>
+                <p className="text-[8px] md:text-xs outline outline-1 md:outline-2 outline-white p-2 text-center">06:45, 15:30</p>
+                <p className="text-[8px] md:text-xs outline outline-1 md:outline-2 outline-white p-2 text-center">06:45, 15:30</p>
+                <p className="text-[8px] md:text-xs outline outline-1 md:outline-2 outline-white p-2 text-center">06:45, 16:30</p>
+                <p className="text-[8px] md:text-xs outline outline-1 md:outline-2 outline-white p-2 text-center">06:45, 10:00, 15:00</p>
+                <p className="text-[8px] md:text-xs outline outline-1 md:outline-2 outline-white p-2 rounded-br-md text-center" >08:00, 16:30</p>
             </div>
         </div>
-        <p className="text-lg font-semibold text-white text-center mt-10">Αγία Μαρίνα - Νέα Στύρα</p>
+        {/*<p className="text-lg font-semibold text-white text-center mt-10">Αγία Μαρίνα - Νέα Στύρα</p>
         <div className="grid w-full shadow-xl items-center mt-2 md:mt-4">
             <div className="grid grid-cols-8 grid rows-3">
                 <div className="w-full md:w-40"></div>
@@ -104,7 +104,7 @@ const Shops = () =>  {
                 <p className="text-[8px] md:text-xs outline outline-1 md:outline-2 outline-white p-2 text-center">9:00, 16:00</p>
                 <p className="text-[8px] md:text-xs outline outline-1 md:outline-2 outline-white p-2 rounded-br-md text-center">9:00, 16:00</p>
             </div>
-        </div>
+        </div>*/}
         <div className="mt-16 bg-white bg-opacity-10 rounded-md shadow-md md:shadow-xl p-2">
                 <h2 className="text-lg font-semibold text-white text-center mb-0">Ο Καιρός στην Κάρυστο</h2>
                 {loading ? (
