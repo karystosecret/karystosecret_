9ae335afc7df70213c87b67425bbeaa68906c962
karystosecret <karystosecret@gmail.com>
2024-11-30 13:56:25 +0200

    third commit2

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,7 @@
 import React, { useEffect, useState } from "react";
 import Image from 'next/image';
 import logoImage from '/public/icons/logofnl.png';
+import logoImage2 from '/public/icons/biglogo.png';
 import { TfiAlignJustify } from "react-icons/tfi";
 import Link from "next/link";
 
@@ -64,8 +65,8 @@ const Header = () =>  {
                 <Link href="/culture" className="hidden md:flex items-center justify-center text-[#7ea9b5] hover:text-[#a0b29d] mx-8 text-xs text-center" >Τοπική Κουλτούρα</Link>
                 <Link href="/events" className="hidden md:flex items-center justify-center text-[#7ea9b5] hover:text-[#a0b29d] mx-8 text-xs text-center" >Εκδηλώσεις</Link>
             </div>
-            <Link href="/" className={header ? "w-44 md:w-32 h-28 md:h-20 hover:opacity-75 absolute top-1 px-4 pt-2 left-0 right-0 m-auto" : "md:w-80 w-44 h-28 md:h-40 hover:opacity-75 absolute top-1 md:top-4 px-4 md:pt-0 pt-2 left-0 right-0 m-auto"}>
-                <Image className="m-auto" layout="intrinsic" src={logoImage} width={200} height={160} alt=""/>
+            <Link href="/" className={header ? "w-44 md:w-20 h-28 md:h-20 hover:opacity-75 absolute top-1 px-4 pt-2 left-0 right-0 m-auto" : "md:w-80 w-44 h-28 md:h-40 hover:opacity-75 absolute top-1 md:top-4 px-4 md:pt-0 pt-2 left-0 right-0 m-auto"}>
+                <Image className="m-auto" layout="intrinsic" src={logoImage2} width={100} height={160} alt=""/>
             </Link>
         </div>    
     );
